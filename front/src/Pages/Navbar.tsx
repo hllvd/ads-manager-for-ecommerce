bfaed43b01261ef94006c4e6554182067b46477c
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2023-01-02 21:38:41 -0300

    isolate  page content

--- front/src/Pages/Navbar.tsx
+++ front/src/Pages/Navbar.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import { Copyright } from '../Components/Copyright/Copyright';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Chart from '../Components/Chart';
-import Deposits from '../Components/Deposits';
-import Orders from '../Components/Orders';
+import Box from '@mui/material/Box';
+
 import { TopMenu } from '../Components/Menu-top/TopMenu';
 import { MenuLeft } from '../Components/Menu-left/MenuLeft';
+import { MainContent } from '../Components/Main-content/MainContent';
+import { DashboardPage } from './Dashboard';
 
 
 const drawerWidth: number = 240;
@@ -32,57 +28,10 @@ function DashboardContent() {
         <CssBaseline />
         <TopMenu open={open} toggledrawer={toggleDrawer} drawerwidth={drawerWidth} />
         <MenuLeft open={open} toggledrawer={toggleDrawer} drawerwidth={drawerWidth} />
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} teste="Moaee" />
-          </Container>
-        </Box>
+        <MainContent>
+          <DashboardPage />
+          <Copyright sx={{ pt: 4 }} teste="Moaee" />
+        </MainContent>
       </Box>
     </ThemeProvider>
   );
